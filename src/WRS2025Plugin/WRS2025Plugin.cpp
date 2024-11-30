d0e5c693107d8995afa89893a9fb7d9afe314171
k38-suzuki <suzuki.kenta38@jaea.go.jp>
2024-11-30 14:23:33 +0900

    Release ver.0.0.0

---
+++ src/WRS2025Plugin/WRS2025Plugin.cpp
@@ -0,0 +1,38 @@
+/**
+   @author Kenta Suzuki
+*/
+
+#include <cnoid/Format>
+#include <cnoid/Plugin>
+#include <cnoid/WRSUtilBar>
+
+using namespace cnoid;
+
+class WRS2025Plugin : public Plugin
+{
+public:
+    WRS2025Plugin() : Plugin("WRS2025")
+    {
+        require("Body");
+        require("WRSUtil");
+    }
+    
+    virtual bool initialize() override
+    {
+        WRSUtilBar::instance()->addFormat({ "WRS2025", 2.0 });
+        return true;
+    }
+
+    virtual const char* description() const override
+    {
+        static std::string text =
+            formatC("WRS2025 Plugin Version {}\n", CNOID_FULL_VERSION_STRING) +
+            "\n" +
+            "Copyright (c) 2024 Japan Atomic Energy Agency.\n"
+            "\n" +
+            MITLicenseText();
+        return text.c_str();
+    }
+};
+
+CNOID_IMPLEMENT_PLUGIN_ENTRY(WRS2025Plugin)
